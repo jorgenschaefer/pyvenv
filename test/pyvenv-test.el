097194ea22aee5969379ec30ed4e247d2c6c636b
Jorgen Schaefer <contact@jorgenschaefer.de>
2014-04-11 22:29:57 +0200

    Remove hack to change PYTHONHOME.

--- test/pyvenv-test.el
+++ test/pyvenv-test.el
@@ -15,8 +15,6 @@
            (pyvenv-post-activate-hooks
             (list (lambda ()
                     (setq post-activate-venv pyvenv-virtual-env)))))
-      (setq process-environment (cons "PYTHONHOME=/usr"
-                                      process-environment))
       (pyvenv-activate tmpdir)
       (should (equal pre-activate-venv tmpdir))
       (should (equal post-activate-venv tmpdir))
