2e884f5907aa63da81bc4f1a94df36950680b077
Jorgen Schaefer <contact@jorgenschaefer.de>
2014-05-30 14:03:48 +0200

    Bump version to 1.2.

--- pyvenv.el
+++ pyvenv.el
@@ -4,7 +4,7 @@
 
 ;; Author: Jorgen Schaefer <contact@jorgenschaefer.de>
 ;; URL: http://github.com/jorgenschaefer/pyvenv
-;; Version: 1.1
+;; Version: 1.2
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License
