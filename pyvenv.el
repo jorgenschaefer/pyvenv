5074dd78dd4947956b9dd19dc6284751b9546a7c
Jorgen Schaefer <contact@jorgenschaefer.de>
2018-04-27 11:26:09 +0000

    Bump version: 1.13 → 1.14

--- pyvenv.el
+++ pyvenv.el
@@ -4,7 +4,7 @@
 
 ;; Author: Jorgen Schaefer <contact@jorgenschaefer.de>
 ;; URL: http://github.com/jorgenschaefer/pyvenv
-;; Version: 1.13
+;; Version: 1.14
 ;; Keywords: Python, Virtualenv, Tools
 
 ;; This program is free software; you can redistribute it and/or
